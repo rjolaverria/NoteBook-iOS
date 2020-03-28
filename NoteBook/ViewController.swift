ce5aba3e7b186e81d9ad7f95c4ea7cca30df637c
Rafi Olaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-03-28 14:59:24 -0400

    Initial Commit

---
+++ NoteBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteBook
+//
+//  Created by Rafi Olaverria on 3/28/20.
+//  Copyright © 2020 Rafi Olaverria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
